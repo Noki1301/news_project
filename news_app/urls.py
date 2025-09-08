1e32e955527956f62e114905994b68fc37e6afcd
Noki1301 <133777854+Noki1301@users.noreply.github.com>
2025-09-08 17:15:33 +0500

    templatega bazadan malumotlarni uzatish done

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import news_list, news_detail, HomePageView, contactPageView
+from .views import news_list, news_detail, HomePageView, ContactPageView, HomePageView
 
 urlpatterns = [
-    path("", HomePageView, name="home_page"),
+    path("", HomePageView.as_view(), name="home_page"),
     path("news/", news_list, name="all_news_list"),
     path("all<int:id>/", news_detail, name="news_detail"),
-    path("contact/", contactPageView, name="contact_page"),
+    path("contact/", ContactPageView.as_view(), name="contact_page"),
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -4,6 +4,7 @@ from .models import News, Category
 from django.shortcuts import get_object_or_404
 from .forms import ContactForm
 from django.contrib import messages
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
 
@@ -21,10 +22,45 @@ def news_detail(request, id):
 
 
 def HomePageView(request):
-    news = News.published.all()
+    news = News.published.all().order_by("-publish_time")[:3]
+    maxalliy_news = News.published.all().filter(category__name="Маҳаллий")[:3]
+    sport_news = News.published.all().filter(category__name="Спорт")[:3]
+    technology_news = News.published.all().filter(category__name="Технология")[:3]
+    external_news = News.published.all().filter(category__name="Хориж")[:3]
     categories = Category.objects.all()
-    context = {"news": news, "categories": categories}
+    context = {
+        "news": news,
+        "categories": categories,
+        "maxalliy_news": maxalliy_news,
+        "sport_news": sport_news,
+        "technology_news": technology_news,
+        "external_news": external_news,
+    }
     return render(request, "news/index.html", context=context)
+
+
+class HomePageView(ListView):
+    model = News
+    template_name = "news/index.html"
+    context_object_name = "news"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        context["news"] = self.model.published.all().order_by("-publish_time")[:3]
+        context["maxalliy_news"] = self.model.published.all().filter(
+            category__name="Маҳаллий"
+        )[:3]
+        context["sport_news"] = self.model.published.all().filter(
+            category__name="Спорт"
+        )[:3]
+        context["technology_news"] = self.model.published.all().filter(
+            category__name="Технология"
+        )[:3]
+        context["external_news"] = self.model.published.all().filter(
+            category__name="Хориж"
+        )[:3]
+        return context
 
 
 def contactPageView(request):
@@ -33,11 +69,29 @@ def contactPageView(request):
     return render(request, "news/contact.html", context=context)
 
 
-def contactPageView(request):
-    form = ContactForm(request.POST or None)
-    if request.method == "POST" and form.is_valid():
-        form.save()
-        messages.success(request, "Biz bilan bog'langaningiz uchun rahmat!")
-        return redirect("home_page")
-    context = {"form": form}
-    return render(request, "news/contact.html", context=context)
+# def contactPageView(request):
+#     form = ContactForm(request.POST or None)
+#     if request.method == "POST" and form.is_valid():
+#         form.save()
+#         messages.success(request, "Biz bilan bog'langaningiz uchun rahmat!")
+#         return redirect("home_page")
+#     context = {"form": form}
+#     return render(request, "news/contact.html", context=context)
+
+
+class ContactPageView(TemplateView):
+    template_name = "news/contact.html"
+
+    def get(self, request, *args, **kwargs):
+        form = ContactForm()
+        context = {"form": form}
+        return render(request, self.template_name, context=context)
+
+    def post(self, request, *args, **kwargs):
+        form = ContactForm(request.POST)
+        if request.method == "POST" and form.is_valid():
+            form.save()
+            messages.success(request, "Biz bilan bog'langaningiz uchun rahmat!")
+            return redirect("home_page")
+        context = {"form": form}
+        return render(request, self.template_name, context=context)
